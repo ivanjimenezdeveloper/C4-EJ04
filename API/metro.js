2eccbc1a11b5bbf6ce9600d5479ae538b972d958
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-02 12:53:43 +0200

    existeLinea done

--- API/metro.js
+++ API/metro.js
@@ -1,4 +1,38 @@
 require("dotenv").config();
-urlMetro = process.env.API_METRO;
+const fetch = require("node-fetch");
 
-console.log(urlMetro);
+const urlMetro = process.env.API_METRO;
+const apiId = process.env.API_ID;
+const apiKey = process.env.API_KEY;
+const urlMetroAutenticado = `${urlMetro}?app_id=${apiId}&app_key=${apiKey}`;
+
+const getLineasMetro = async () => {
+  const respuesta = await fetch(urlMetroAutenticado);
+  const paradas = await respuesta.json();
+
+  return paradas.features;
+};
+
+const comprobarLinea = async (linea, lineas) => {
+  const lineaReturn = lineas.filter((lineaApi) =>
+    lineaApi.properties.NOM_LINIA.toLowerCase() === linea.toLowerCase()
+      ? lineaApi
+      : ""
+  );
+
+  return lineaReturn || false;
+};
+
+const existeLinea = async (lineaRespuesta) => {
+  const lineas = await getLineasMetro();
+  const linea = await comprobarLinea(lineaRespuesta, lineas);
+
+  if (!linea) {
+    return -1;
+  }
+  console.log(linea);
+};
+
+exports = {
+  existeLinea,
+};
