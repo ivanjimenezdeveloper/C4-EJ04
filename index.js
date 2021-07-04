9a5d0dce9c1aa127d3060fce64e92d5da4de2525
Sandra Cuéllar <sandra.cllp.22@gmail.com>
2021-07-04 02:17:50 +0200

    actualizacion

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { program } = require("commander");
+const chalk = require("chalk");
+const {tipoTransporte} = require ("./cuestionarioTMB");
+const api = require ("./API/metro");
+const mail = require ("./email/mail");
 
 (async () => {
   program.option("-c, --color <color>", "Especifica un color en hexadecimal");
@@ -6,4 +10,10 @@ const { program } = require("commander");
   program.parse(process.argv);
 
   const { color, abrev } = program.opts();
+  const respuesta = await tipoTransporte();
+  if(respuesta.tipo === "bus"){
+    console.log(chalk.yellow("No tenemos información disponible sobre los buses https://www.tmb.cat/es/home"));
+    process.exit(0)}
+
+   console.log(await api.existeLinea(respuesta.consultaLinea));
 })();
